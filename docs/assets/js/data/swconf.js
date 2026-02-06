12005c4198a03cd423059796a2e8a8681a18fb3b
Ibrahim Khalil <ibrahimkhalil@OTLTPRD051.local>
2026-02-06 19:12:55 +0530

    ics link added and commits reversed to fix the site

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770385298',resources: [
+    cacheName: 'chirpy-1770385368',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
