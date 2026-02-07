b6e77caf45bfbce536c9560b639cb9ffba11812a
Ibrahim Khalil <ibrahimkhalil@OTLTPRD051.local>
2026-02-07 05:39:04 +0530

    capstone projects, vansh article

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770422898',resources: [
+    cacheName: 'chirpy-1770422939',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
