1543cc65256b787061d32b259768d69ca4c9fd2c
Ibrahim Khalil <ibrahimkhalil@OTLTPRD051.local>
2026-02-08 22:28:46 +0530

    alumni data added

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770569670',resources: [
+    cacheName: 'chirpy-1770569923',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
