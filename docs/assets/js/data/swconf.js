39e5248f565f2c818572288767deeca725f0eba3
Ibrahim Khalil <ibrahimkhalil@OTLTPRD051.local>
2026-02-08 23:02:12 +0530

    ibrahim md

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770571754',resources: [
+    cacheName: 'chirpy-1770571926',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
